3023d0b1deb20431275da18c454257f79b9b4ff9
Evan Hayes <hayesevan7@gmail.com>
2022-03-25 16:04:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
 import "./App.css";
 import React, { useState } from "react";
-import data from "./components/data";
 import Header from "./components/Header";
 import { BrowserRouter as Router } from "react-router-dom";
 import Paths from "./components/Paths";
 import Footer from "./components/Footer";
+import irons from "./components/images/irons.jpeg";
+import driver from "./components/images/driver.jpg";
+import putter from "./components/images/putter.jpeg";
+import wedges from "./components/images/wedges.jpeg";
+import pants from "./components/images/pants.jpeg";
+import shirt from "./components/images/shirt.png";
+import balls from "./components/images/balls.jpeg";
+import bag from "./components/images/bag.webp";
+const data = {
+  productItems: [
+    {
+      id: "1",
+      name: "Iron Set",
+      price: 1299,
+      image: irons,
+    },
+    {
+      id: "2",
+      name: "Driver",
+      price: 349,
+      image: driver,
+    },
+    {
+      id: "3",
+      name: "Putter",
+      price: 129,
+      image: putter,
+    },
+    {
+      id: "4",
+      name: "Wedges",
+      price: 499,
+      image: wedges,
+    },
+    {
+      id: "5",
+      name: "Golf Pants",
+      price: 79,
+      image: pants,
+    },
+    {
+      id: "6",
+      name: "Golf Shirts",
+      price: 59,
+      image: shirt,
+    },
+    {
+      id: "7",
+      name: "Golf Balls",
+      price: 49,
+      image: balls,
+    },
+    {
+      id: "8",
+      name: "Golf Bags",
+      price: 199,
+      image: bag,
+    },
+  ],
+};
 
 function App() {
   const { productItems } = data;
